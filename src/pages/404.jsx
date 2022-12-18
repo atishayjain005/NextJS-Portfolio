c07fd3be4262388b4e118129ca9c1ad550d196a4
Atishay Jain <atishay005@gmail.com>
2022-12-18 16:25:18 +0530

    404 page added

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,3 +1,13 @@
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div
+      style={{ backgroundColor: "black", height: "100vh" }}
+      className="d-flex flex-column justify-content-center align-items-center text-light "
+    >
+      <h1 className="display-1 fw-bold">404 - Page Not Found</h1>
+      <h4>
+        ...maybe the page you&apos;re looking for is not found or never existed.
+      </h4>
+    </div>
+  );
 }
